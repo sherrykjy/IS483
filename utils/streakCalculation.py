b299319f50c735eceab2706e2b8ff7747dfe1689
xuxinhui16 <xinhui.xu.2021@scis.smu.edu.sg>
2024-10-05 20:53:09 +0800

    Added complex microservice streakCalculation.py

--- utils/streakCalculation.py
+++ utils/streakCalculation.py
@@ -5,44 +5,73 @@ import requests
 from invokes import invoke_http
 from os import environ
 import hashlib
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
 
-user_URL = "http://localhost:5001/user"
+goal_URL = "http://localhost:5008/goal"
+streak_URL = "http://localhost:5010/streak"
+mvp_URL = "http://localhost:5020/estimate_mvpa"
 
-@app.route('/update_streak_if_mvpa/<int:user_id>/<int:goal_id>/<int:week>', methods=['GET'])
-def update_streak_if_mvpa(user_id, goal_id, week):
-    # Fetch the goal
-    goal = Goal.query.get(goal_id)
-    if not goal:
-        return jsonify({"error": "Goal not found"}), 404
-
-    # Call the MVPA estimation microservice
-    mvpa_response = requests.get(f'http://localhost:5020/estimate_mvpa/{user_id}/{week}')
-    if mvpa_response.status_code != 200:
-        return jsonify({'error': 'Failed to estimate MVPA'}), 400
-
-    mvpa_data = mvpa_response.json()
-    mvpa_minutes = mvpa_data.get('mvpa_minutes', 0)
-
-    # Compare the MVPA with the goal's target
-    if mvpa_minutes >= goal.target:
-        # Fetch the user's streak for the current goal and week
-        streak = Streak.query.filter_by(goal_id=goal_id, week_started=week).first()
-        if streak:
-            streak.streak_count += 1  # Increment the streak count
-            streak.week_current = week
-        else:
-            # If no streak exists for the goal, create a new one
-            streak = Streak(goal_id=goal_id, week_started=week, week_current=week, streak_count=1)
-
+@app.route('/update_streak/<int:user_id>/<int:goal_id>', methods=['GET'])
+def update_streak_if_mvpa():
+    if request.is_json:
         try:
-            db.session.add(streak)
-            db.session.commit()
-            return jsonify(streak.json()), 200
+            streak_information = request.get_json()
+            print("\nReceived login information in JSON:", streak_information)
+            result = processStreakInformation(streak_information)
+            return jsonify(result), result['code']
+        
         except Exception as e:
-            db.session.rollback()
-            return jsonify({"error": str(e)}), 400
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+            
+            return jsonify({
+                "code": 500,
+                "message": "user_login.py internal error: " + ex_str
+            }), 500
+
+def processStreakInformation(streak_information):
+    
+    goal_id = streak_information['goal_id']
+    user_id = streak_information['user_id']
+    streak_id = streak_information['streak_id']
+    
+    # Get Goal of User
+    goal_result = invoke_http(f"{goal_URL}/{user_id}", method='GET')
+    
+    # Get Estimated MET
+    met_result = invoke_http(f"{mvp_URL}")
+    # Get Streak 
+    
+    estimated_met = met_result["met"]
+    streak_json= None
+    
+    if (estimated_met >= 3) and (goal_result["target"]):
+        streak_result = invoke_http(f"{streak_URL}/{user_id}")
+        
+        if streak_result["week_current"] + 1 == datetime.date(datetime.now()).isocalendar()[1]:
+            streak_json = {
+                "streak_id": streak_result.streak_id,
+                "goal_id": streak_result.goal_id,
+                "week_started": streak_result.week_started,
+                "week_current": datetime.date(datetime.now()).isocalendar()[1],
+                "streak_count": streak_result.streak_count + 1
+                }
     else:
-        return jsonify({"message": "MVPA does not meet the target"}), 200
+        streak_json = {
+            "streak_id": streak_result.streak_id,
+            "goal_id": streak_result.goal_id,
+            "week_started": datetime.date(datetime.now()).isocalendar()[1],
+            "week_current": datetime.date(datetime.now()).isocalendar()[1],
+            "streak_count": 1
+            }
+        
+    streak_update_result = invoke_http(f"{streak_URL}/{streak_id}", method='PUT', json=streak_json)
+    
+    return streak_update_result
+            
+        
