5bd3abf4b9c14150abcca6233d52176b9ec52fab
dg0h <danellegoh.2021@scis.smu.edu.sg>
2024-10-06 03:22:40 +0800

    linked join events and booked events

--- h365/src/views/bookedEventsPage.vue
+++ h365/src/views/bookedEventsPage.vue
@@ -24,73 +24,70 @@
             <div class="filterButton">
                 <datePicker />
             </div>
-        </div>
+        </div>        
 
-        <div class="basicHeader">
-            <p>2 September 2024</p>    
-        </div>
-
-        <div class="divider"></div>
-
-
-        <!-- v-for however many events there are -->
-        <router-link :to="{ name: 'viewEventPage'}">
-                                    
-            <div class="basicCard">
-                <div class="cardImage">
-                    <img src="../assets/icons/events/event1.png">
-                </div>
-
-                <div class="cardText">
-                    <!-- v-if few slots left -->
-                    <div class="lowSlotAlert">
-                        Few Slots Left
-                    </div>
-
-                    <!-- programme name -->
-                    <p class="programmeName">Active Family Programme</p>
-
-                    <!-- activity name -->
-                    <p class="eventName">Pilates</p>
-
-                    <!-- date, day, and time  -->
-                    <div class="eventInfo">
-                        <i class="uil uil-schedule eventIcon"></i>
-                        <div class=eventDetails>
-                            <p>2 September 2024, Monday</p>
-                            <p>6.15pm - 7.15pm</p>
-                        </div>
-                    </div>
-
-                    <!-- location -->
-                    <div class="eventInfo">
-                        <i class="uil uil-map-pin eventIcon"></i>
-                        <div class=eventDetails>
-                            <p>PAYA LEBAR, Paya Lebar Precinct (Paya Lebar Quarter Mall)</p>
-                        </div>
-                    </div>
-
-                    <div class="eventBtnIntensity">
-                        <form action="">
-                            <button class="bookEventBtn">Book Now</button>
-                        </form>
-
-                        <!-- intensity -->
-                        <div class="intensity">
-                            <p>Intensity: </p>
-                            <img src="../assets/icons/events/intensity1.png">
-                        </div>
-                    </div>
-                </div>
+        <!-- loop for each date -->
+        <div v-for="date in sortedDates" :key="date">
+            <!-- date header -->
+            <div class="basicHeader">
+                <p>{{ formattedDateHeader(date) }}</p>
             </div>
-        </router-link>
+            <div class="divider"></div>
+            <div v-for="event in eventData[date]" :key="event.event_id">
+                <router-link :to="{ name: 'viewEventPage', params: { eventId: event.event_id } }">
+                    <div class="basicCard">
+                        <div class="cardImage">
+                            <img src="../assets/icons/events/event1.png">
+                        </div>
+                        <div class="cardText">
+                            <!-- v-if few slots left -->
+                            <div class="lowSlotAlert" v-if="event.slots_left <= 5">
+                                Few Slots Left
+                            </div>
+                            <!-- programme name -->
+                            <p class="programmeName" v-if="event.event_program != 'Null'">{{ event.event_program }}</p>
+                            <!-- activity name -->
+                            <p class="eventName">{{ event.title }}</p>
+                            <!-- date, day, and time  -->
+                            <div class="eventInfo">
+                                <i class="uil uil-schedule eventIcon"></i>
+                                <div class=eventDetails>
+                                    <p>{{ formattedDate(event.start_date) }}</p>
+                                    <p>{{ formattedTime(event.start_date, event.end_date) }}</p>
+                                </div>
+                            </div>
+                            <!-- location -->
+                            <div class="eventInfo">
+                                <i class="uil uil-map-pin eventIcon"></i>
+                                <div class=eventDetails>
+                                    <p>{{ event.location }}</p>
+                                </div>
+                            </div>
+                            <div class="eventBtnIntensity">
+                                <form action="">
+                                    <button class="bookEventBtn">Book Now</button>
+                                </form>
+                                <!-- intensity -->
+                                <div class="intensity">
+                                    <p>Intensity: </p>
+                                    <img v-if="event.tier === 1" src="../assets/icons/events/intensity1.png">
+                                    <img v-else-if="event.tier === 2" src="../assets/icons/events/intensity2.png">
+                                    <img v-else-if="event.tier === 3" src="../assets/icons/events/intensity3.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
     </div>
 
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed } from "vue";
 import datePicker from '../components/datePicker.vue';
+import { useStore } from 'vuex';
 
 export default 
     defineComponent({
@@ -98,10 +95,111 @@ export default
             datePicker
         },
         setup() {
+            console.log("booked events page");
             const selectedTab = ref('bookedEvents');
+            const store = useStore(); // Import useStore from vuex
+            const userId = computed(() => store.state.userId); // Access userId from the store
+            const userEmail = computed(() => store.state.userEmail) // Access userEmail from the store
+
             return {
-                selectedTab
+                selectedTab,
+                userId,
+                userEmail
             };
+        },
+        data() {
+            return {
+                sortedDates: [],
+                eventData: {}
+            }
+        },
+        async mounted() {
+            this.$http.get("http://127.0.0.1:5007/userevent/active/" + this.userId)
+            .then(response => {
+                var eventDataResponse = response.data;
+                console.log(eventDataResponse);
+                this.eventData = {};
+                this.sortedDates = [];
+                for (var i = 0; i < eventDataResponse.length; i++) {
+                    // console.log(eventDataResponse[i]);
+                    let data = eventDataResponse[i].data;
+                    // console.log(data);
+                    let event_id = data["event_id"];
+                    let current_signups = data["current_signups"];
+                    let max_signups = data["max_signups"];
+                    let slots_left = max_signups - current_signups;
+                    let event_program = data["event_program"];
+                    let title = data["title"];
+                    let start_date = data["start_date"];
+                    let end_date = data["end_date"];
+                    let location = data["location"];
+                    let tier = data["tier"];
+
+                    let date_key = start_date.split("T")[0];
+
+                    if (!this.eventData[date_key]) {
+                        this.eventData[date_key] = [];
+                        this.sortedDates.push(date_key);
+                    }
+
+                    this.eventData[date_key].push({
+                        "event_id": event_id,
+                        "current_signups": current_signups,
+                        "max_signups": max_signups,
+                        "slots_left": slots_left,
+                        "event_program": event_program,
+                        "title": title,
+                        "start_date": start_date,
+                        "end_date": end_date,
+                        "location": location,
+                        "tier": tier
+                    })
+
+                }
+                this.sortedDates.sort();
+                console.log("event data:", this.eventData);
+                console.log("sorted dates:", this.sortedDates);
+            })
+            .catch(error => {
+                console.log("error:", error);
+            });
+        },
+        methods: {
+            // Format date as '2 September 2024, Monday'
+            formattedDate(dateStr) {
+                const date = new Date(dateStr);
+
+                // Get the formatted day, month, and year
+                const day = date.getDate(); // 1
+                const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date); // August
+                const year = date.getFullYear(); // 2024
+                const weekday = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date); // e.g., Wednesday
+
+                return `${day} ${month} ${year}, ${weekday}`;
+            },
+            formattedDateHeader(dateStr) {
+                const date = new Date(dateStr);
+                return date.toLocaleDateString('en-GB', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            },
+            // Format time as '10.00am - 11.00am'
+            formattedTime(startDateStr, endDateStr) {
+                const startDate = new Date(startDateStr);
+                const endDate = new Date(endDateStr);
+
+                const formatTime = date => {
+                    return new Intl.DateTimeFormat('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true
+                    }).format(date);
+                };
+
+                return `${formatTime(startDate)} - ${formatTime(endDate)}`;
+            }
         }
     });
 </script>
